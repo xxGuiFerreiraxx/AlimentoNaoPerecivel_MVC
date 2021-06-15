83d9fbdff28a393a5bfff58fb22d13e175e35624
xxGuiFerreiraxx <gui39011982@gmail.com>
2021-06-15 16:34:49 -0300

    Adicionar arquivos de projeto.

---
+++ AlimentoNaoPerecivel/Controllers/AlimentoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AlimentoNaoPerecivel.Models;
+
+namespace AlimentoNaoPerecivel.Controllers
+{
+    public class AlimentoController : Controller
+    {
+        // GET: Alimento
+        public ActionResult Index()
+        {
+            var alimento = new Alimento();
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Index(Alimento alimento)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", alimento);
+            }
+            return View(alimento);
+        }
+        public ActionResult Resultado (Alimento alimento)
+        {
+            
+            
+                return View("Resultado", alimento);
+           
+          
+        }
+
+        public ActionResult LoginUnico(string codigo)
+        {
+            var bdAlimento = new Collection<string>
+            {
+                "4",
+                "9"
+            };
+            return Json(bdAlimento.All(x => x.ToLower() != codigo.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AlimentoNaoPerecivel/Models/Alimento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web;
+
+namespace AlimentoNaoPerecivel.Models
+{
+    public class Alimento
+    {
+        [Required(ErrorMessage = "O campo Código é obrigatório")]
+        [Remote("LoginUnico", "alimento", ErrorMessage = "Login existente.")]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Descrição é obrigatório")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Insira de 5 a 50 caracteres!")]
+        public string Descricao { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage ="Valor inválido")]
+        public string Peso { get; set; }
+
+        [Required(ErrorMessage = "O campo Categoria é obrigatório")]
+        public string Categoria { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Valor inválido")]
+        [Required(ErrorMessage = "O campo ValorUnit é obrigatório")]
+        public string ValorUnit { get; set; }
+    }
+}
